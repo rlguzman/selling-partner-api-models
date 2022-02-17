2484b7a976ca36b58bfe712cd20bf9f608f033c0
Robert <rlguzman.developer@gmail.com>
2022-02-16 22:40:54 -0800

    Add AWSAuthenticationCredentialsProvider

--- clients/sellingpartner-api-aa-csharp/src/Amazon.SellingPartnerAPIAA/AWSAuthenticationCredentialsProvider.cs
+++ clients/sellingpartner-api-aa-csharp/src/Amazon.SellingPartnerAPIAA/AWSAuthenticationCredentialsProvider.cs
@@ -1,22 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Amazon.SellingPartnerAPIAA
 {
-  public class AWSAuthenticationCredentialsProvider
-  {
-    /**
-     * AWS IAM Role ARN
-     */
-    private string _roleArn;
+    public class AWSAuthenticationCredentialsProvider
+    {
+        /**
+            * AWS IAM Role ARN
+            */
+        private string _roleArn;
 
-    /**
-     * AWS IAM Role Session Name
-     */
-    private string _roleSessionName;
+        /**
+            * AWS IAM Role Session Name
+            */
+        private string _roleSessionName;
 
-    public string roleArn { get => _roleArn; set => _roleArn = value; }
-    public string roleSessionName { get => _roleSessionName; set => _roleSessionName = value; }
-  }
+        public string roleArn { get => _roleArn; set => _roleArn = value; }
+        public string roleSessionName { get => _roleSessionName; set => _roleSessionName = value; }
+
+        public AWSAuthenticationCredentialsProvider()
+        {
+            _roleSessionName = Guid.NewGuid().ToString();
+        }
+    }
 }
